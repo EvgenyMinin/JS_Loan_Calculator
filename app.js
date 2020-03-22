7aff37ace30998a0791d0a71431166d956a214be
Johnny1992 <evgeny.frontend@gmail.com>
2020-03-22 19:06:31 +0300

    added loader

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const showError = error => {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "none";
   const errorDiv = document.createElement("div");
   const card = document.querySelector(".card");
   const heading = document.querySelector(".heading");
@@ -13,9 +15,7 @@ const showError = error => {
   }, 3000);
 };
 
-const calculateResults = e => {
-  e.preventDefault();
-
+const calculateResults = () => {
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
   const years = document.getElementById("years");
@@ -34,11 +34,18 @@ const calculateResults = e => {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    document.getElementById("results").style.display = "block";
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers");
   }
 };
 
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", e => {
+  e.preventDefault();
+
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+});
